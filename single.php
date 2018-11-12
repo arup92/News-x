d6d23c8447ecc0c0a8da5131ad4bef13982c6e93
subam <subam@subams-MacBook-Air.local>
2018-11-12 18:30:40 +0530

    single page

--- single.php
+++ single.php
@@ -20,6 +20,23 @@
 					<?php endif; ?>
 				<div class="single-post-text">
 					<?php echo the_content(); ?>
+
+					<div class="author-info">
+						<div class="author-image clearfix">
+				  			<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ) ); ?>"><?php echo get_avatar( get_the_author_meta('ID') ); ?></a>
+						</div><!-- /.author-image -->
+						<div class="comment-wrapper">
+								<b class="comment-name"><?php esc_html_e( 'Published By', 'writer-blog' );?> <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ) ); ?>"><?php the_author(); ?></a></b>
+								<?php if ( get_the_author_meta( 'description' ) ): ?>
+									<p><?php the_author_meta( 'description' ); ?></p>
+								<?php endif ?>
+							<div class="author-link">
+								<?php if ( get_the_author_meta( 'user_url' ) ): ?>
+									<a href="<?php the_author_meta( 'user_url' ); ?>"><?php esc_html_e( 'Visit Website', 'writer-blog' );?></a>
+								<?php endif ?>
+							</div><!-- /.author-link -->
+						</div><!-- /.comment-wrapper -->
+					</div><!-- /.author-info -->
 				</div><!-- /.single-post-text -->	
 			</div><!-- /.row .eight columns -->
 				<?php get_sidebar(); ?>
@@ -30,4 +47,13 @@
 	endwhile;
 	endif;
 	wp_reset_postdata();
+?>
+	<div class="container comment-section">
+		<div class="row">
+			<div class="eight columns">
+				<?php comments_template();?>
+			</div><!-- /.row .eight columns -->
+		</div><!-- /.row -->	
+	</div><!-- /.container -->
+<?php
 get_footer();
