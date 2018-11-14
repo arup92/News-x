72972de300101d90ad2998c540f227d259bed120
subam <subam@subams-MacBook-Air.local>
2018-11-14 19:03:01 +0530

    push

--- archive.php
+++ archive.php
@@ -6,51 +6,41 @@
 get_header();
 ?>
 
-<?php if ( is_category() ): ?>
-<div class="u-full-width">
-   <div class="row">
-    	<div class="category-header-background" style="background-image: linear-gradient( to bottom, rgba(0, 0, 0, .5 ) 0%, rgba( 0, 0, 0, .5 ) 100% ), url( '<?php echo esc_url( writer_blog_category_image() ); ?>' )">
-    		<div class="category-header-content">
-    			<div class="category-title">
-    				<h1><?php echo single_cat_title(); ?></h1>
-    			</div><!-- /.category-title -->
-    			<?php echo category_description(); ?>
-    		</div><!-- /.header-content -->
-    	</div>
-   </div> <!-- /.row -->
-</div> <!-- /.u-full-width -->
-<?php 
-	else:
-		writer_blog_breadcrumb_switcher();
-	endif;
-?>
-<div class="container <?php echo $check_margin = ( is_category() ) ? 'post-loop-margin' : 'no-header-bg' ; ?> body-container">
-	<div class="row">
-		<div class="eight columns">
-			<?php if ( have_posts() ) : 
-			
-			/* Start the Loop */
+<div class="container section-five">
+
+    <!-- /.section-title-red -->
+    <div class="row">
+    	<div class="eight columns">
+        <?php
+		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
+		?>
+		<div class="one-third block shadow">
+            <div class="fullwidth-post-section">
+                <?php
+                	if ( has_post_thumbnail() ):
+						the_post_thumbnail( 'full' );
+					endif;
+				?> 
+				<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+                <p><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ) ); ?>"><span class="author"><?php the_author(); ?></span></a></a> <?php echo esc_html( get_the_date() ); ?></p>
 
-				get_template_part( 'template-parts/post/content', get_post_format() );
-
-			endwhile;
-
-				the_posts_pagination( array(
-					'mid_size'  => 2,
-					'prev_text' => __( '&nbsp;', 'writer-blog' ),
-					'next_text' => __( '&nbsp;', 'writer-blog' ),
-				) );
-
+                <?php
+					$trim_excerpt = get_the_excerpt();
+					$short_excerpt = wp_trim_words( $trim_excerpt, $num_words = 15, $more = '... ' );
+				?>
+                <p class="post-excerpt"><?php echo esc_html( $short_excerpt ); ?></p>
+            </div><!-- /.big-cat-section -->
+        </div>
+		<?php
+				endwhile;
 			else :
-
 				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif; ?>
-		</div><!-- /.eight .columns -->
-
+			endif;
+		?>
+		</div><!-- /.eight columns -->
 		<?php get_sidebar(); ?>
-	</div><!-- /.row -->
-</div><!-- /.container .home-post-loop-->
+    </div><!-- /.row -->
+</div>
 
 <?php get_footer(); ?>
