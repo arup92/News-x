827101d3a009448268ffbb33fdc6710871fc59f1
subam <subam@subams-MacBook-Air.local>
2018-11-15 19:05:53 +0530

    posh

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <div class="search">
-	<form role="search" method="get" action="<?php echo home_url('/'); ?>">
+	<form class="searchform" role="search" method="get" action="<?php echo home_url('/'); ?>">
 		<input type="search" class="searchTerm" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 		<button type="submit" class="searchButton">
 			<i class="fa fa-search"></i>
